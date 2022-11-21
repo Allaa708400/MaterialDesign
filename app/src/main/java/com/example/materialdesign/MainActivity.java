7602443a5604834b6cc0e7a10eede7e55ff46bf8
zead allaa <allaa15@yahoo.com>
2022-11-21 20:24:07 +0400

    Initial commit

---
+++ app/src/main/java/com/example/materialdesign/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.materialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Menu;
+
+@SuppressLint({"MissingInflatedId", "LocalSuppress"})
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+             Toolbar toolbar = findViewById(R.id.toolbar);
+             setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+}
